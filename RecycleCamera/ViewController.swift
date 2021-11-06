116be9b54ae35744f0060b48c97f520944e8825b
micah-kim <85715752+micah-kim@users.noreply.github.com>
2021-11-07 07:26:55 +0900

    Add files via upload

---
+++ RecycleCamera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RecycleCamera
+//
+//  Created by Micah Kim on 11/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
